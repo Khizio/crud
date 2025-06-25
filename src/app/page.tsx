1a4fad8b38fcd38b9192d28e4933f6a05ee58fe0
Khizio <khizarna123@gmail.com>
2025-06-25 05:39:55 +0500

    HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Welcome
-    </div>
+     <link rel="stylesheet" href="https://crud-q7q3sde0r-khizzzs-projects-52cac738.vercel.app/student/create" />
+       <link rel="stylesheet" href="https://crud-q7q3sde0r-khizzzs-projects-52cac738.vercel.app/student/list" /> 
+         <link rel="stylesheet" href="https://crud-q7q3sde0r-khizzzs-projects-52cac738.vercel.app/student/1" />  
+    
+  </div>
   );
 }
