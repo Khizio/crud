8eee8855e4fa1df3ec80559aa50015cd55f1f23b
Khizio <khizarna123@gmail.com>
2025-06-25 05:43:30 +0500

    HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-     <link rel="stylesheet" href="https://crud-q7q3sde0r-khizzzs-projects-52cac738.vercel.app/student/create" />
-       <link rel="stylesheet" href="https://crud-q7q3sde0r-khizzzs-projects-52cac738.vercel.app/student/list" /> 
-         <link rel="stylesheet" href="https://crud-q7q3sde0r-khizzzs-projects-52cac738.vercel.app/student/1" />  
-    
-  </div>
+      Welcome
+      to start 
+      https://khizars22csc012finalexam-8wmo51hly-khizzzs-projects-52cac738.vercel.app/student/create
+      https://khizars22csc012finalexam-8wmo51hly-khizzzs-projects-52cac738.vercel.app/student/list
+      https://khizars22csc012finalexam-8wmo51hly-khizzzs-projects-52cac738.vercel.app/student/1
+    </div>
   );
 }
